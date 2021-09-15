af89206ec01ddd1f1510ab6095497524a2f55f21
Al Piepho <alpiepho@yahoo.com>
2021-09-15 13:49:21 -0600

    attempt to add uart to combo.py

--- feather_nrf52840_express/combo.py
+++ feather_nrf52840_express/combo.py
@@ -39,48 +39,59 @@ while True:
 
     while ble.connected:
         if uart_service.in_waiting:
-            packet = Packet.from_stream(uart_service)
-            if isinstance(packet, ButtonPacket):
-                if packet.pressed:
-                    if packet.button == ButtonPacket.BUTTON_1:
-                        # The 1 button was pressed.
-                        print("1 button pressed!")
-                    if packet.button == ButtonPacket.BUTTON_2:
-                        # The 2 button was pressed.
-                        print("2 button pressed!")
-                    if packet.button == ButtonPacket.BUTTON_3:
-                        # The 3 button was pressed.
-                        print("3 button pressed!")
-                    if packet.button == ButtonPacket.BUTTON_4:
-                        # The 4 button was pressed.
-                        print("4 button pressed!")
-                    elif packet.button == ButtonPacket.UP:
-                        # The UP button was pressed.
-                        print("UP button pressed!")
-                    elif packet.button == ButtonPacket.DOWN:
-                        # The DOWN button was pressed.
-                        print("DOWN button pressed!")
-                    elif packet.button == ButtonPacket.LEFT:
-                        # The LEFT button was pressed.
-                        print("LEFT button pressed!")
-                    elif packet.button == ButtonPacket.RIGHT:
-                        # The RIGHT button was pressed.
-                        print("RIGHT button pressed!")
-            elif isinstance(packet, ColorPacket):
-                print(packet.color)
-                pixels.fill(packet.color)
-            elif isinstance(packet, AccelerometerPacket):
-                print("Acceleration:", packet.x, packet.y, packet.z)
-            elif isinstance(packet, MagnetometerPacket):
-                print("Magnetometer:", packet.x, packet.y, packet.z)
-            elif isinstance(packet, GyroPacket):
-                print("Gyro:", packet.x, packet.y, packet.z)
-            elif isinstance(packet, QuaternionPacket):
-                print("Quaternion:", packet.x, packet.y, packet.z)
-            elif isinstance(packet, LocationPacket):
-                print("Latitude:", packet.latitude)
-                print("Longitude", packet.longitude)
-                print("Altitude:", packet.altitude)
+            try:
+                packet = Packet.from_stream(uart_service)
+                if isinstance(packet, ButtonPacket):
+                    if packet.pressed:
+                        if packet.button == ButtonPacket.BUTTON_1:
+                            # The 1 button was pressed.
+                            print("1 button pressed!")
+                        if packet.button == ButtonPacket.BUTTON_2:
+                            # The 2 button was pressed.
+                            print("2 button pressed!")
+                        if packet.button == ButtonPacket.BUTTON_3:
+                            # The 3 button was pressed.
+                            print("3 button pressed!")
+                        if packet.button == ButtonPacket.BUTTON_4:
+                            # The 4 button was pressed.
+                            print("4 button pressed!")
+                        elif packet.button == ButtonPacket.UP:
+                            # The UP button was pressed.
+                            print("UP button pressed!")
+                        elif packet.button == ButtonPacket.DOWN:
+                            # The DOWN button was pressed.
+                            print("DOWN button pressed!")
+                        elif packet.button == ButtonPacket.LEFT:
+                            # The LEFT button was pressed.
+                            print("LEFT button pressed!")
+                        elif packet.button == ButtonPacket.RIGHT:
+                            # The RIGHT button was pressed.
+                            print("RIGHT button pressed!")
+                elif isinstance(packet, ColorPacket):
+                    print(packet.color)
+                    pixels.fill(packet.color)
+                elif isinstance(packet, AccelerometerPacket):
+                    print("Acceleration:", packet.x, packet.y, packet.z)
+                elif isinstance(packet, MagnetometerPacket):
+                    print("Magnetometer:", packet.x, packet.y, packet.z)
+                elif isinstance(packet, GyroPacket):
+                    print("Gyro:", packet.x, packet.y, packet.z)
+                elif isinstance(packet, QuaternionPacket):
+                    print("Quaternion:", packet.x, packet.y, packet.z)
+                elif isinstance(packet, LocationPacket):
+                    print("Latitude:", packet.latitude)
+                    print("Longitude", packet.longitude)
+                    print("Altitude:", packet.altitude)
+            except Exception as err:
+                # try uart
+                s = uart_service.readline()
+                if s:
+                    try:
+                        result = s #str(eval(s))
+                    except Exception as e:
+                        result = repr(e)
+                    uart_service.write(result.encode("utf-8"))
+
 
     # If we got here, we lost the connection. Go up to the top and start
     # advertising again and waiting for a connection.
