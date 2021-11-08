4ab14848b5dd0375d96eda5cb6daa581cd5b6e08
Al Piepho <alpiepho@yahoo.com>
2021-11-08 06:01:09 -0800

    more cli

--- feather_nrf52840_express/mock_cli.py
+++ feather_nrf52840_express/mock_cli.py
@@ -1,7 +1,9 @@
 
 import random
-from threading import Thread
-from time import sleep
+import threading
+import time
+
+
 # from adafruit_ble import BLERadio
 # from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 # from adafruit_ble.services.nordic import UARTService
@@ -12,7 +14,9 @@ from time import sleep
 
 samples = {}
 count = 0
-rate = 1
+count_max = 100
+count_rollover = 0
+count_rate = 1
 pause = False
 
 result = None
@@ -21,35 +25,47 @@ def sample_thread():
     global count
       
     while True:
-        sleep(rate)
+        time.sleep(count_rate)
         if not pause:
-            samples[count] = random.uniform(0, 3.3)
+            seconds = time.time()
+            if count >= count_max:
+                count_rollover = count_rollover + 1
+                count = 0
+                samples = {} # TODO: create rollover instead of reset
+
+            samples[seconds] = random.uniform(0, 3.3)
             count = count + 1
 
-# possible options
-#     0  help
-#     1  info
-#     2  sample rate (seconds)
-#     3  start
-#     4  pause
-#     5  stop
-#     6  dump
-#     7  reset
-#     8  alarm enable
-#     9  alarm high enable
-#     10 alarm high threshold
-#     11 alarm low enable
-#     12 alarm high threshold
-#     13 alarm led enable
-#     14 alarm beacon enable
-#     15 last as C
-#     16 last as F
-#     17 last as raw
-
 def Help():
-    print('0, h, ? - help')
-    print('1, i - info')
-    print('7, r - reset')
+    # help
+    print('0,h,? - help')
+    print('1,i   - info ')
+    # measure
+    print('10 - last raw/C/F')
+    print('11 - last distance raw/cm/inches')
+    # sampling
+    print(f'20 - max samples (default {count_max})')
+    print(f'21 - sample rate (default {count_rate})')
+    print('22,s - start')
+    print('23,t - stop')
+    print('24,p - pause')
+    print('25,r - reset')
+    # dump
+    print('30 - dump to serial - raw')
+    print('31 - dump to serial - C')
+    print('32 - dump to serial - F (default)')
+    # alarms
+    print('40 - battery led slow blink (default 1)')
+    print('41 - threshold alarm led fast blink (default 1)')
+    # triggers
+    print('50 - temp high threshold - raw (default TBD)')
+    print('51 - temp high threshold - C   (default TBD)')
+    print('52 - temp high threshold - F   (default TBD)')
+    print('53 - temp high seconds         (default TBD)')
+    print('60 - temp low  threshold - raw (default TBD)')
+    print('61 - temp low  threshold - C   (default TBD)')
+    print('62 - temp low  threshold - F   (default TBD)')
+    print('63 - temp low  seconds         (default TBD)')
 
 def Info():
     print(f'rate: {rate}')
@@ -68,7 +84,7 @@ def Reset():
 
 
 # Setup
-t = Thread(target=sample_thread)
+t = threading.Thread(target=sample_thread)
 t.start()
 
 while True:
@@ -78,8 +94,49 @@ while True:
         Help()
     elif option == '1' or option == 'i':
         Info()
-    elif option == '7' or option == 'r':
+    elif option == '10':
+        LastTemp()
+    elif option == '11':
+        LastDistance()
+    elif option.starts_with('20 '): # max samples <count>
+        try:
+            parts = options.split()
+            count_max = parts[1]
+        except:
+            print('bad value')
+    elif option.starts_with('20 '): # sample rate <seconds>
+        try:
+            parts = options.split()
+            count_rate = parts[1]
+        except:
+            print('bad value')
+    elif option == '22' or option == 's':
+        Start()
+    elif option == '23' or option == 't':
+        Stop()
+    elif option == '24' or option == 'p':
+        Pause()
+    elif option == '25' or option == 'r':
         Reset()
+
+    # # dump
+    # print('30 - dump to serial - raw')
+    # print('31 - dump to serial - C')
+    # print('32 - dump to serial - F (default)')
+    # # alarms
+    # print('40 - battery led slow blink (default 1)')
+    # print('41 - threshold alarm led fast blink (default 1)')
+    # # triggers
+    # print('50 - temp high threshold - raw (default TBD)')
+    # print('51 - temp high threshold - C   (default TBD)')
+    # print('52 - temp high threshold - F   (default TBD)')
+    # print('53 - temp high seconds         (default TBD)')
+    # print('60 - temp low  threshold - raw (default TBD)')
+    # print('61 - temp low  threshold - C   (default TBD)')
+    # print('62 - temp low  threshold - F   (default TBD)')
+    # print('63 - temp low  seconds         (default TBD)')
+
+
 
 #     ble.start_advertising(advertisement)
 #     print("Waiting to connect")
