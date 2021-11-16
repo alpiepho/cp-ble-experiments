b5200ffcbb5c857b69f0f0e88d3d30e9d8a6935a
apiepho <alpiepho@gmail.com>
2021-11-16 07:01:07 -0700

    started temp turnon

--- feather_nrf52840_express/temp_sensor1.py
+++ feather_nrf52840_express/temp_sensor1.py
@@ -4,6 +4,7 @@ import random
 import time
 
 from digitalio import DigitalInOut, Direction, Pull
+from analogio import AnalogIn
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
@@ -12,10 +13,16 @@ from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.color_packet import ColorPacket
 
+import adafruit_dht
+
 # Setup
 ble = BLERadio()
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
+
+dhtDevice = adafruit_dht.DHT22(board.D5)
+
+batt_monitor = AnalogIn(board.BATTERY)
 
 samples = {}
 
@@ -28,6 +35,14 @@ led.direction = Direction.OUTPUT
 led_last_time = time.time()
 led_time_delta = 500000000 # 1/2 sec
 
+temp_c_trigger_value = 1000
+temp_c_trigger_seconds = 10
+
+# TODO - add options for controlling temp alarm
+# TODO - add temp check TODO need pin and pull up
+# TODO - add battery check 
+
+
 def Help(uart):
     # help
     # NOTE: try to favor letters over numbers, 
@@ -35,42 +50,42 @@ def Help(uart):
     #       those are easier from Bluefruit Connect app
     msg1 = f'h - help\n'
     msg2 = f'i - info\n'
-    #msg3 = f'a - toggle led_alarm (current {led_alarm})\n'
-    #msg4 = f'b - toggle led_battery (current {led_battery})\n'
+    msg3 = f't - set temp_c_trigger_value (current {temp_c_trigger_value})\n'
+    msg4 = f'T - set temp_c_trigger_seconds (current {temp_c_trigger_seconds})\n'
     msg5 = f'd - dump sample to serial\n'
     uart.write(msg1.encode("utf-8"))
     uart.write(msg2.encode("utf-8"))
-    #uart.write(msg3.encode("utf-8"))
-    #uart.write(msg4.encode("utf-8"))
+    uart.write(msg3.encode("utf-8"))
+    uart.write(msg4.encode("utf-8"))
     uart.write(msg5.encode("utf-8"))
 
-# def getInt(option, oldValue):
-#     result = oldValue
-#     try:
-#         parts = option.split()
-#         if parts[1].startswith(b't') or parts[1].startswith(b'T'):
-#             result = 1
-#         elif parts[1].startswith(b'f') or parts[1].startswith(b'F'):
-#             result = 0
-#         else:
-#             result = int(parts[1])
-#     except:
-#         print('bad value')
-#     return result
+def getInt(option, oldValue):
+    result = oldValue
+    try:
+        parts = option.split()
+        if parts[1].startswith(b't') or parts[1].startswith(b'T'):
+            result = 1
+        elif parts[1].startswith(b'f') or parts[1].startswith(b'F'):
+            result = 0
+        else:
+            result = int(parts[1])
+    except:
+        print('bad value')
+    return result
 
-# def getFloat(option, oldValue):
-#     result = oldValue
-#     try:
-#         parts = option.split()
-#         result = float(parts[1])
-#     except:
-#         print('bad value')
-#     return result
+def getFloat(option, oldValue):
+    result = oldValue
+    try:
+        parts = option.split()
+        result = float(parts[1])
+    except:
+        print('bad value')
+    return result
 
 def Info(uart):
-    msg1 = f'led_battery: {led_battery}\n'
-    msg2 = f'led_alarm:   {led_alarm}\n'
-    msg3 = f'led_state:   {led_state}\n'
+    msg1 = f'led_battery:            {led_battery}\n'
+    msg2 = f'temp_c_trigger_value:   {temp_c_trigger_value}\n'
+    msg3 = f'temp_c_trigger_seconds: {temp_c_trigger_seconds}\n'
     uart.write(msg1.encode("utf-8"))
     uart.write(msg2.encode("utf-8"))
     uart.write(msg3.encode("utf-8"))
@@ -93,6 +108,8 @@ def SetLed(state):
 def RunConfigMode(uart):
     global led_battery
     global led_alarm
+    global temp_c_trigger_value
+    global temp_c_trigger_seconds
 
     option = uart.readline()
     option = option.strip()
@@ -102,8 +119,10 @@ def RunConfigMode(uart):
         led_alarm = not led_alarm
     elif option == b'b':
         led_battery = not led_battery
-    # elif option.startswith(b'b '):
-    #     led_battery = getInt(option, led_battery)
+    elif option.startswith(b't '):
+        temp_c_trigger_value = getFloat(option, temp_c_trigger_value)
+    elif option.startswith(b'T '):
+        temp_c_trigger_seconds = getInt(option, temp_c_trigger_seconds)
     elif option == b'd':
         Dump(uart)
     elif len(option) > 0:
@@ -150,8 +169,37 @@ def RunLed():
             led_state = 0
             SetLed(0)
 
+last_temp_ts = time.monotonic_ns
 def RunDHT():
+    # global temp_c_trigger_value
+    # global temp_c_trigger_seconds
+    # global led_alarm
+    # global last_temp_ts
+
+    # # FROM: https://circuitpython.readthedocs.io/projects/dht/en/latest/
+    # temp_c = dhtDevice.temperature
+    # temp_f = temp_c * (9 / 5) + 32
+    # humidity = dhtDevice.humidity
+    # print("Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(temp_f, temp_c, humidity))
+    # ts = time.monotonic_ns
+    # samples[ts] = temp_c
+    # led_alarm = 0
+    # if (ts - last_temp_ts)/1000000000 > temp_c_trigger_seconds:
+    #     last_temp_ts = ts
+    #     if temp_c > temp_c_trigger_value:
+    #         led_alarm = 1
     ...
+
+def RunBattery():
+    global led_battery
+    global batt_monitor
+
+    voltage = (batt_monitor.value / 65535.0) * 3.3 * 2
+    #print("Battery: {:.1f} V ".format(voltage))
+    led_battery = 0
+    if voltage < 3.0:
+        led_battery = 1
+
 
 
 while True:
@@ -159,61 +207,13 @@ while True:
     print("Waiting to connect")
     while not ble.connected:
         RunLed()
+        RunDHT()
+        RunBattery()
     print("Connected")
     while ble.connected:
         RunLed()
+        RunDHT()
+        RunBattery()
         RunConfigMode(uart)
 
-# TODO - add battery check
-# TODO - add temp check
-# TODO - add options for controlling temp alarm
 
-# From https://github.dev/adafruit/Adafruit_CircuitPython_MagTag peripherals.py
-# from analogio import AnalogIn
-# _batt_monitor = AnalogIn(board.BATTERY)
-# self._batt_monitor.deinit()
-# def battery(self):
-#     """Return the voltage of the battery"""
-#     return (self._batt_monitor.value / 65535.0) * 3.3 * 2
-
-
-
-# FROM: https://circuitpython.readthedocs.io/projects/dht/en/latest/
-
-# # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# 2 # SPDX-License-Identifier: MIT
-# 3
-# 4 import time
-# 5 import board
-# 6 import adafruit_dht
-# 7
-# 8 # Initial the dht device, with data pin connected to:
-# 9 dhtDevice = adafruit_dht.DHT22(board.D18)
-# 10
-# 11 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# 12 # This may be necessary on a Linux single board computer like the Raspberry Pi,
-# 13 # but it will not work in CircuitPython.
-# 14 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
-# 15
-# 16 while True:
-# 17 try:
-# 18 # Print the values to the serial port
-# 19 temperature_c = dhtDevice.temperature
-# 20 temperature_f = temperature_c * (9 / 5) + 32
-# 21 humidity = dhtDevice.humidity
-# 22 print(
-# 23 "Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(
-# 24 temperature_f, temperature_c, humidity
-# 25 )
-# 26 )
-# 27
-# 28 except RuntimeError as error:
-# 29 # Errors happen fairly often, DHT's are hard to read, just keep going
-# 30 print(error.args[0])
-# 31 time.sleep(2.0)
-# 32 continue
-# 33 except Exception as error:
-# 34 dhtDevice.exit()
-# raise error
-# 36
-# 37 time.sleep(2.0)
